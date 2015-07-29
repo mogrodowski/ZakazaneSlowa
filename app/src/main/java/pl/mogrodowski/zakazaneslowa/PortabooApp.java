31e9b0a0da8cfb4782cefdebd2b9eabc61949cff
mogrodowski <kontakt@mogrodowski.pl>
2015-07-29 22:12:57 +0200

    Add PortabooApp

---
+++ app/src/main/java/pl/mogrodowski/zakazaneslowa/PortabooApp.java
@@ -0,0 +1,36 @@
+package pl.mogrodowski.zakazaneslowa;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+public class PortabooApp extends Application {
+
+    private ConnectivityManager cMgr;
+
+    //
+    // lifecycle
+    //
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        cMgr = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+
+    public boolean connectionPresent() {
+        NetworkInfo netInfo = cMgr.getActiveNetworkInfo();
+        if ((netInfo != null) && (netInfo.getState() != null)) {
+            return netInfo.getState().equals(NetworkInfo.State.CONNECTED);
+        }
+        return false;
+    }
+}
